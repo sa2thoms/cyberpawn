10de654c4ed11e6db8f41a3b3cb5ed9bbecb3787
Simon Thomson <simon1.thomson@gmail.com>
2021-02-14 00:55:56 -0500

    Added another test

--- cyberpawnService/cyberpawnTest/ChessGameFunctionsTest.cpp
+++ cyberpawnService/cyberpawnTest/ChessGameFunctionsTest.cpp
@@ -13,3 +13,22 @@ TEST(ChessGameFunctionsTest, makeMoveIfLegalMakesE4OpeningMove) {
     EXPECT_EQ(resultPosition.value()[e4Move.from], PieceCode::noPiece);
     EXPECT_EQ(resultPosition.value()[e4Move.to], PieceCode::whitePawn);
 }
+
+TEST(ChessGameFunctionsTest, aSeriesOfPawnMovesWorksCorrectly) {
+    ChessPosition position;
+    std::vector<ChessMove> moves = {
+        { {4, 1}, {4, 3} },
+        { {4, 6}, {4, 5} },
+        { {3, 1}, {3, 3} },
+        { {3, 6}, {3, 4} }
+    };
+
+    for (const auto & move : moves) {
+        auto newPosition = makeMoveIfLegal(position, move);
+        EXPECT_TRUE(newPosition.has_value());
+        position = newPosition.value();
+    }
+
+    EXPECT_EQ(position[(ChessSquare{ 4, 5 })], PieceCode::blackPawn);
+    EXPECT_EQ(position[(ChessSquare{ 3, 3 })], PieceCode::whitePawn);
+}
